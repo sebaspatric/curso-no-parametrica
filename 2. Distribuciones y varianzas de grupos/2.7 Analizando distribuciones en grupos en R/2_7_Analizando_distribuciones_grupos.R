d007118d5f40ec37811e0d165d7063ef5d2a45dc
sebaspatric <sebastian.menares@gmail.com>
2026-04-15 04:34:25 +0000

    ruta grupo

--- 2. Distribuciones y varianzas de grupos/2.7 Analizando distribuciones en grupos en R/2_7_Analizando_distribuciones_grupos.R
+++ 2. Distribuciones y varianzas de grupos/2.7 Analizando distribuciones en grupos en R/2_7_Analizando_distribuciones_grupos.R
@@ -2,8 +2,9 @@ library(tidyverse)
 library(broom)
 theme_set(theme_classic())
 library(readr)
-iq <- read_csv("~/A CURSO NO PARAMETRICA/CLASES/2. Distribuciones y varianzas de grupos/2.7 Analizando distribuciones en grupos en R/iq.csv")
+iq <- read_csv("2. Distribuciones y varianzas de grupos/2.7 Analizando distribuciones en grupos en R/iq.csv")
 View(iq)
+
 
 grupo1=iq[iq$School==1,]
 grupo2=iq[iq$School==2,]
@@ -20,7 +21,7 @@ x.test2
 p_value_thresh=0.05
 
 
-#Definiendo la función que me devuelve si se cumple o no la hipotesis
+#Definiendo la funci?n que me devuelve si se cumple o no la hipotesis
 sw_test_results <- function(x.test,p_value_thresh) {
   if(x.test$p.value > p_value_thresh){
     print('Assumption satisfied')
@@ -32,8 +33,8 @@ sw_test_results <- function(x.test,p_value_thresh) {
   
 }
 
-#Definiendo la función del grafico de distribucion Normal
-plotn <- function(x,main="Histograma de frecuencias \ny distribución normal",
+#Definiendo la funci?n del grafico de distribucion Normal
+plotn <- function(x,main="Histograma de frecuencias \ny distribuci?n normal",
                   xlab="X",ylab="Densidad") {
   min <- min(x)
   max <- max(x)
@@ -45,13 +46,13 @@ plotn <- function(x,main="Histograma de frecuencias \ny distribución normal",
 
 #Probando la funcion sobre res1 
 sw_test_results(x.test1,p_value_thresh)
-plotn(res1,main="Distribución normal")
+plotn(res1,main="Distribuci?n normal")
 #Probando la funcion sobre res2 
 sw_test_results(x.test2,p_value_thresh)
-plotn(res2,main="Distribución normal")
+plotn(res2,main="Distribuci?n normal")
 
 #########################################
-#Otro tipo de estimación de densidad
+#Otro tipo de estimaci?n de densidad
 #Res1
 df <- data.frame(
   res1
